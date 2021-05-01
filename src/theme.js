f7f70ef3335e7805d8c40ec80b9f85b9ce4e32ea
isaac-a <isaac.amos89@gmail.com>
2021-05-01 15:15:19 +0300

    change background and delete redundant icons

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const lightTheme = {
-  bg1: "#6dc0ec",
+  bg1: "#0093E9",
   bg2: "#eee",
 };
 
@@ -12,8 +12,7 @@ const darkTheme = {
 };
 
 export const darkBg = "linear-gradient(179.4deg, #070D20 11.4%, #1E2B56 70.2%)";
-export const lightBg =
-  "linear-gradient(179.4deg, #6dc0ec 22.3%, #bff1ec 84.1%)";
+export const lightBg = "linear-gradient(179.4deg, #0093E9 0%, #80D0C7 100%";
 
 export const getTheme = (isDarkMode) => {
   const palette = isDarkMode ? darkTheme : lightTheme;
